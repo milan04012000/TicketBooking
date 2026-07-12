120c0df193b3f88dfd73841d11bcda4bbf32c816
Milan Pal <milan04012000@gmail.com>
2026-07-12 16:06:39 +0530

    11

--- TicketBooking/Login/LoginViewModel.swift
+++ TicketBooking/Login/LoginViewModel.swift
@@ -14,7 +14,7 @@ class LoginViewModel:NSObject{
         
         let parameters: [String: Any] = [
             "username": "bbbbbbbbb"
-            "pass": "aaa"
+            "pass": "aaa1"
         ]
         
         let headers: HTTPHeaders = [
